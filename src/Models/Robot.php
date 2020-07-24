1185424ddd46dad0e3882190d2c4e4387183915a
Alex Mac <alex@mcbain.net.au>
2020-07-25 00:05:31 +1000

    Update comment in Robots

--- src/Models/Robot.php
+++ src/Models/Robot.php
@@ -255,7 +255,7 @@ class Robot
     }
 
     /**
-     * Report the Robot's current position and direction
+     * Report the Robot's current position and direction.
      *
      * @return string
      * @throws RobotException
