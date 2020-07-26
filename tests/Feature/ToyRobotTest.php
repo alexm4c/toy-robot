bceb748b2d771de652bc50fa050555043a31a1b4
Alex Mac <alex@mcbain.net.au>
2020-07-26 13:14:16 +1000

    Update ToyRobotTest, handle null result value

--- tests/Feature/ToyRobotTest.php
+++ tests/Feature/ToyRobotTest.php
@@ -13,30 +13,34 @@ class ToyRobotTest extends TestCase
     {
         $command = __DIR__ . '/../../toy-robot.php commands/corner-to-corner';
         $result = `$command`;
+        $result = $result ? trim($result) : null;
 
-        $this->assertEquals(trim($result), '4,4,EAST');
+        $this->assertEquals($result, '4,4,EAST');
     }
 
     public function testCanRunSpinLeft()
     {
         $command = __DIR__ . '/../../toy-robot.php commands/spin-left';
         $result = `$command`;
+        $result = $result ? trim($result) : null;
 
-        $this->assertEquals(trim($result), '0,0,SOUTH');
+        $this->assertEquals($result, '0,0,SOUTH');
     }
 
     public function testCanRunSpinRight()
     {
         $command = __DIR__ . '/../../toy-robot.php commands/spin-right';
         $result = `$command`;
+        $result = $result ? trim($result) : null;
 
-        $this->assertEquals(trim($result), '0,0,SOUTH');
+        $this->assertEquals($result, '0,0,SOUTH');
     }
 
     public function testCanRunOutOfBounds()
     {
         $command = __DIR__ . '/../../toy-robot.php commands/out-of-bounds';
         $result = `$command`;
+        $result = $result ? trim($result) : null;
 
         $this->assertEquals(trim($result), '4,0,SOUTH');
     }
